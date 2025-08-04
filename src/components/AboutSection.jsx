cd3849b37fe7a32eff29b7ddb7af7c8083cd7d69
Thefahad674 <fahadf30511@gmail.com>
2025-08-04 17:02:44 +0530

    done

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="/Fahad-CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-all duration-300  hover:shadow-[0_0_10px_rgba(139,92,246,0.5)]
          hover:scale-105 active:scale-95"
               >
